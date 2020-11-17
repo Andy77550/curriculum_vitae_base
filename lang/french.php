0fd0a1198653eb2636cfdebff0a51aab2fcbeb15
Andy77550 <74592653+Andy77550@users.noreply.github.com>
2020-11-17 11:20:59 +0100

    Add files via upload

---
+++ lang/french.php
@@ -0,0 +1,4 @@
+<?php
+$strings['plugin_title'] = "Curriculum_Vitae_Base";
+$strings['plugin_comment'] = "Ce plugin permet aux administrateurs de créer un curriculum_vitae_base";
+$strings['enable_plugin_curriculum_vitae_base'] = "Activer le plugin";
