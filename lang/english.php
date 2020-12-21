1959eb2fb8e6e96b45fdb2dba3611af456b1d73a
Andy77550 <74592653+Andy77550@users.noreply.github.com>
2020-12-21 14:27:42 +0100

    Add files via upload

--- lang/english.php
+++ lang/english.php
@@ -1,6 +1,6 @@
 <?php
 $strings['plugin_title'] = "Curriculum_Vitae_Base";
-$strings['plugin_comment'] = "This plugin allows the admins to create a curriculum_vitae_base ";
+$strings['plugin_comment'] = "This plugin allows the learners to create a CV";
 $strings['enable_plugin_curriculum_vitae_base'] = "Enable plugin";
 
 
--- lang/french.php
+++ lang/french.php
@@ -1,4 +1,4 @@
 <?php
 $strings['plugin_title'] = "Curriculum_Vitae_Base";
-$strings['plugin_comment'] = "Ce plugin permet aux administrateurs de créer un curriculum_vitae_base";
+$strings['plugin_comment'] = "Ce plugin permet aux apprenants de créer un CV";
 $strings['enable_plugin_curriculum_vitae_base'] = "Activer le plugin";
