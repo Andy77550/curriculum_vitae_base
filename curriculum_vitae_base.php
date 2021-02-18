f1a3df0e422c99edda7e958b798dff0e4ccc8fa1
Andy77550 <74592653+Andy77550@users.noreply.github.com>
2021-02-18 14:45:57 +0100

    Add files via upload

---
+++ curriculum_vitae_base.php
@@ -0,0 +1,103 @@
+<?php
+/* For licensing terms, see /license.txt */
+
+/**
+ * Class curriculum_vitae_base.
+ */
+class curriculum_vitae_base extends Plugin
+{
+    /**
+     * curriculum_vitae_base constructor.
+     */
+    protected function __construct()
+    {
+        parent::__construct(
+            '1.0',
+            'Andy Vespuce',
+            [
+                'enable_plugin_curriculum_vitae_base' => 'boolean',
+            ]
+        );
+    }
+
+    /**
+     * @return curriculum_vitae_base|null
+     */
+    public static function create()
+    {
+        static $result = null;
+
+        return $result ? $result : $result = new self();
+    }
+	
+
+	
+	
+
+    /**
+     * Installation process.
+     */
+    public function install()
+    {
+        $sql = "CREATE TABLE IF NOT EXISTS plugin_curriculum_vitae_base (
+                id INT NOT NULL AUTO_INCREMENT,
+				id_user INT NOT NULL,
+				date_birthday DATE NOT NULL,
+				phone VARCHAR(50) NOT NULL,
+                address VARCHAR(50) NOT NULL,
+                skill_profil VARCHAR(255) NOT NULL,
+                PRIMARY KEY (id));
+        ";
+        Database::query($sql);
+		
+		$sql2 = "CREATE TABLE IF NOT EXISTS experience_pro (
+                id INT NOT NULL AUTO_INCREMENT,
+				id_user INT NOT NULL,
+				type INT NOT NULL,
+				date_begin VARCHAR(50) NOT NULL,
+				date_end VARCHAR(50) NOT NULL,
+                title VARCHAR(255) NOT NULL,
+				description VARCHAR(512) NOT NULL,
+                PRIMARY KEY (id));
+        ";
+        Database::query($sql2);
+
+        $sql3 = "CREATE TABLE IF NOT EXISTS experience_form (
+                id INT NOT NULL AUTO_INCREMENT,
+                id_user INT NOT NULL,
+                type INT NOT NULL,
+                date_begin VARCHAR(50) NOT NULL,
+                date_end VARCHAR(50) NOT NULL,
+                diplome VARCHAR(255) NOT NULL,
+                lieu VARCHAR(255) NOT NULL,
+                etablissement VARCHAR(255) NOT NULL,
+                PRIMARY KEY (id));
+        ";
+        Database::query($sql3);
+
+         $sql4 = "CREATE TABLE IF NOT EXISTS experience_hobbies (
+                id INT NOT NULL AUTO_INCREMENT,
+                id_user INT NOT NULL,
+                type INT NOT NULL,
+                title VARCHAR(255) NOT NULL,
+                description VARCHAR(512) NOT NULL,
+                PRIMARY KEY (id));
+        ";
+        Database::query($sql4);
+    }
+
+    /**
+     * Uninstall process.
+     */
+    public function uninstall()
+    {
+        $sql = "DROP TABLE IF EXISTS plugin_curriculum_vitae_base";
+		$sql2 = "DROP TABLE IF EXISTS experience_pro ";
+        $sql3 = "DROP TABLE IF EXISTS experience_form ";
+        $sql4 = "DROP TABLE IF EXISTS experience_hobbies ";
+        Database::query($sql);
+		Database::query($sql2);
+        Database::query($sql3);
+        Database::query($sql4);
+    }
+}
---
+++ experience_form.php
@@ -0,0 +1,134 @@
+<?php
+/* For licensing terms, see /license.txt */
+
+require_once __DIR__.'/../../main/inc/global.inc.php';
+require_once __DIR__.'/curriculum_vitae_base.php';
+
+
+//api_protect_admin_script();
+
+$plugin = curriculum_vitae_base::create();
+
+$tables = 'experience_form';
+$sql3 = "SELECT * FROM $tables ";
+$result = Database::query($sql3);
+$xpForm = Database::store_result($result, 'ASSOC');
+
+$action = isset($_GET['action']) ? Security::remove_XSS($_GET['action']) : 'add';
+$id_user =  api_get_user_id();
+
+$xpForm= null;
+if (!empty($id_user)) {
+    $sql3 = "SELECT * FROM $tables WHERE id = $id_user AND id_user = $id_user";
+    $result = Database::query($sql3);
+    $xpForm = Database::fetch_array($result, 'ASSOC');
+	
+}
+
+
+
+$form = new FormValidator('add', 'post', api_get_self().'?action='.$action.'&id_user='.$id_user);
+$form->addText('date_begin', $plugin->get_lang('Date de début'));
+$form->addText('date_end', $plugin->get_lang('Date de fin'));
+$form->addText('diplome', $plugin->get_lang('Diplôme'), true);
+$form->addText('lieu', $plugin->get_lang('Lieu'), true);
+$form->addText('etablissement', $plugin->get_lang('Etablissement'), true);
+$form->addButtonSave(get_lang('Save'));
+
+switch ($action) {
+    case 'add':
+        if ($form->validate()) {
+            $values = $form->getSubmitValues();
+            $params = [
+		        'id' => api_get_user_id(),
+		        'id_user' => api_get_user_id(),
+				'type' => 1,
+                'date_begin' => $values['date_begin'],
+                'date_end' => $values['date_end'],
+		        'diplome' => $values['diplome'],
+                'lieu' => $values['lieu'],
+			    'etablissement' => $values['etablissement'],
+            ];
+            $result = Database::insert($tables, $params);
+            if ($result) {
+                Display::addFlash(Display::return_message(get_lang('La formation a été ajoutée !!')));
+            }
+            header('Location: '.api_get_self());
+			header('Location: display-cv.php' );
+            exit;
+        }
+        break;
+		
+		
+		case 'edit':
+			$form->setDefaults($xpForm);
+			if ($form->validate()) {
+				$values = $form->getSubmitValues();
+				$params = [
+					'id' => api_get_user_id(),
+					'id_user' => api_get_user_id(),
+					'date_begin' => $values['date_begin'],
+					'date_end' => $values['date_end'],
+					'diplome' => $values['diplome'],
+					'lieu' => $values['lieu'],
+					'etablissement' => $values['etablissement'],
+				];
+				Database::update($tables, $params, ['id_user = ?' => $id_user]);
+				Display::addFlash(Display::return_message(get_lang('Mise à jour de la formation !!')));
+
+				header('Location: '.api_get_self());
+				header ('Location: cv.php' );
+				exit;
+			}
+		break;
+		
+			
+		case 'return':
+			$form->setDefaults($xpForm);
+			if ($form->validate()) {
+				$values = $form->getSubmitValues();
+				$params = [
+					'id' => api_get_user_id(),
+					'id_user' => api_get_user_id(),
+					'date_begin' => $values['date_begin'],
+					'date_end' => $values['date_end'],
+					'diplome' => $values['diplome'],
+					'lieu' => $values['lieu'],
+					'etablissement' => $values['etablissement'],
+				];
+				Database::update($tables, $params, ['id_user = ?' => $id_user]);
+				header('Location: '.api_get_self());
+				header ('Location: display-cv.php' );
+				exit;
+			}
+		break;
+			
+		case 'delete':
+			if (!empty($xpForm)) {
+				Database::delete($tables, ['id_user = ?' => $id_user]);
+				Display::addFlash(Display::return_message(get_lang('Formation supprimée !')));
+				header('Location: '.api_get_self());
+				header('Location: cv.php' );
+				exit;
+			}
+		break;
+		
+
+
+		case 'cancel':
+			if (empty($xpForm)) {
+				Database::delete($tables, ['id_user = ?' => $id_user]);
+				header('Location: '.api_get_self());
+				exit;
+			}
+		break;
+}
+
+$tpl = new Template($plugin->get_lang('Formation'));
+$tpl->assign('xpForm', $xpForm);
+$tpl->assign('form', $form->returnForm());
+$content = $tpl->fetch('/'.$plugin->get_name().'/view/for-cv.tpl');
+// Assign into content
+$tpl->assign('content', $content);
+// Display
+$tpl->display_one_col_template();
