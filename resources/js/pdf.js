2db9cd66d1b7d03c7ce1727cb9d97d2682fd78ae
Andy77550 <74592653+Andy77550@users.noreply.github.com>
2021-03-25 17:01:18 +0100

    Update pdf.js

--- resources/js/pdf.js
+++ resources/js/pdf.js
@@ -2,26 +2,29 @@ window.onload = function () {
     document.getElementById("download").addEventListener("click", () => {
         const invoice = this.document.getElementById("invoice");
         const none = this.document.getElementById("none").hidden = true;
-        const vide = this.document.getElementById("vide").hidden = true;
-        const disparu = this.document.getElementById("disparu").hidden = true;
-        const empty = this.document.getElementById("empty").hidden = true;
+        const vide_form_add = this.document.getElementById("vide_form_add").hidden = true;
+        var btncache = this.document.getElementsByClassName("btncache");
+
+        for (var i = 0; i < btncache.length; i ++) {
+            btncache[i].style.display = 'none';
+        }
+            
         console.log(invoice);
         console.log(none);
-        console.log(vide);
-        console.log(disparu);
-        console.log(empty);
+        console.log(vide_form_add);
+        console.log(btncache);
         console.log(window);
-        var opt = {
-            margin: 1,
-            filename: 'monCV.pdf',
-            image: { type: 'jpeg', quality: 0.98 },
-            html2canvas: { scale: 2 },
-            jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
-        };
+            var opt = {
+                margin: 0,
+                filename: 'monCV.pdf',
+                image: { type: 'jpeg', quality: 0.98 },
+                html2canvas: { scale: 2 },
+                jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
+            };
         html2pdf().from(invoice).set(opt).save();
         html2pdf().from(none).set(opt).save();
-        html2pdf().from(vide).set(opt).save();
-        html2pdf().from(disparu).set(opt).save();
-        html2pdf().from(empty).set(opt).save();
+        html2pdf().from(vide_form_add).set(opt).save();
+        html2pdf().from(btncache).set(opt).save();
+
     })
 }
